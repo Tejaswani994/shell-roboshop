3835399332d7e57f2881ccb3add55a9fd615aae8
Tejaswani994 <tejaswani.muthyala@gmail.com>
2026-03-19 19:11:42 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,6 @@ DOMAIN_NAME="tejaswini.site" # replace with your domain
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-AMI_ID="ami-0220d79f3f480ecf5"
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0649e7f9dd7766852 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
